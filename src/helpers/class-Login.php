bff03d7e7157f421ed5b80709a4bc352b4e123fe
Daniel <danielramos1981@gmail.com>
2020-05-18 18:10:58 -0400

    Commit files. Added class-Login, help.js, login.js.

--- src/helpers/class-Login.php
+++ src/helpers/class-Login.php
@@ -2,12 +2,20 @@
 
 class Login
 {
-
+    public $user;
     public function _construct() {
-        
+        global $users;
+
+        session_start();
+
+        $this->users = $users;
     }
 
+    public function users_exists() {
 
+
+
+    }
 
 
     public function verify_session() {
