ea00212003d17e3eeb1afdeba292937287bd8bea
Daniel <danielramos1981@gmail.com>
2020-05-19 14:42:55 -0400

    Commit home.php.

---
+++ src/helpers/home.php
@@ -0,0 +1,6 @@
+<?php include('header.php'); ?>
+<div class="wrapper text-center">
+    <h1>welcome <?php echo $user['name']; ?></h1>
+    <p>click <a href="logout.php">here</a>  to logout.</p>
+</div>
+<?php include('footer.php'); ?>
