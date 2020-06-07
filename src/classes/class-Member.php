60f3b4dbc8307b9eb1884a189809f0a6445e42b6
Daniel <danielramos1981@gmail.com>
2020-06-07 19:39:06 -0400

    Commit files. Class-members.php.

---
+++ src/classes/class-Member.php
@@ -0,0 +1,19 @@
+<?php
+class member {
+
+    public string $firstname;
+    public string $lastname;
+    public int $phone;
+    public string $address;
+
+    public function __construct($firstname, $lastname, $phone, $address){
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->phone = $phone; 
+        $this->address = $address;    
+        
+    }
+
+    
+};
+
---
+++ src/classes/class-Trainer.php
@@ -0,0 +1,18 @@
+<?php
+
+class trainer {
+      
+    public string $firstname;
+    public string $lastname;
+    public int $phone;
+    public string $address;
+
+    public function __construct($firstname, $lastname, $phone, $address){
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->phone = $phone; 
+        $this->address = $address;    
+        
+    }
+    
+}
